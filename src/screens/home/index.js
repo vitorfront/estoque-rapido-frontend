5c8c8b76ed4be59b6a372253f32e5706a333145f
vitorfront <vitorrventurini2004@hotmail.com>
2023-11-02 01:45:19 -0300

    feat: create initial logic to fetch the api

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./index.css";
 import { Button, Input } from "../../components";
+import { SAVE_USERNAME_PATH } from "../../services/constants";
 
 export const HomeScreen = () => {
   const navigate = useNavigate();
@@ -14,7 +15,7 @@ export const HomeScreen = () => {
       return;
     }
 
-    localStorage.setItem("@supermarket-list:username", username);
+    localStorage.setItem(SAVE_USERNAME_PATH, username);
     navigate("/list");
   };
 
